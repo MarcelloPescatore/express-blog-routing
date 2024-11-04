77cf8c2d37fd85a35171a2a7e768540da98c6bcb
MarcelloPescatore <pescatoremarcello@gmail.com>
2024-11-04 16:55:34 +0100

    fix error about index

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const app = express();
 const port = 3002;
 // importo le rotte
 const PostsRouter = require('./routing/posts.js');
+// asset publici
+app.use(express.static('public'))
 
 // middleware 
 // app.use(express.json());
