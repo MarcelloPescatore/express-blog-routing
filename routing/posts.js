f7793d26bddd9194b71119814d77e15b462a278e
MarcelloPescatore <pescatoremarcello@gmail.com>
2024-11-04 17:07:33 +0100

    fix positiong about rout show

--- routing/posts.js
+++ routing/posts.js
@@ -5,11 +5,11 @@ const router = express.Router();
 
 // rotta di index
 router.get('/', PostsController.index);
-// rotta di show
-router.get('/:slug', PostsController.show);
 // rotta per tag
 router.get('/filter', PostsController.filter);
 // Crea un nuovo post
 router.post('/store', PostsController.store); 
+// rotta di show
+router.get('/:slug', PostsController.show);
 
 module.exports = router;
